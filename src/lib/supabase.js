b906ac7844b7ccf2a92ec5018b50d6de0532435d
anshulsaxena425565-hue <anshulsaxena425565@gmail.com>
2026-09-23 14:21:21 +0530

    Add Supabase client

---
+++ src/lib/supabase.js
@@ -0,0 +1,4 @@
+import { createClient } from '@supabase/supabase-js'
+export const SUPABASE_URL='https://ejlgobrbajobdbcjodhr.supabase.co'
+export const SUPABASE_KEY='sb_publishable_Y1qSvfxdjUUpIzH6USAl2g_di7FwICk'
+export const supabase=createClient(SUPABASE_URL,SUPABASE_KEY)
